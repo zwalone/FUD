ad2f5b29cc7c8a2014cba481ea710d9cccd27652
Madoxen <pchlad5@gmail.com>
2021-04-28 17:04:51 +0200

    Indent

--- src/storage.js
+++ src/storage.js
@@ -2,36 +2,30 @@
 const storage = window.localStorage;
 
 ///Gets an object or string for the given key from localStorage
-export function getItem(key)
-{
+export function getItem(key) {
     var item = storage.getItem(key)
-    if(item === null)
+    if (item === null)
         return null;
 
     //Try parsing, if failed return normal string
-    try
-    {
+    try {
         return JSON.parse(item)
     }
-    catch(error)
-    {
+    catch (error) {
         return item;
     }
 }
 
 ///Sets an object or string for the given key from localStorage
-export function setItem(key, item)
-{
+export function setItem(key, item) {
     var serializedItem = item
-    if(typeof item !== "string")
-    {
+    if (typeof item !== "string") {
         serializedItem = JSON.stringify(item) //ASK: throw an exception on failure? 
     }
     storage.setItem(key, serializedItem);
 }
 
-export function removeItem(key)
-{
+export function removeItem(key) {
     storage.removeItem(key)
 }
 
--- src/storage.test.js
+++ src/storage.test.js
@@ -1,13 +1,11 @@
 import * as st from './storage';
 
 test('storage sets an item as an object', () => {
-  var testBed ={someniceProp: "hhee"};
+  var testBed = { someniceProp: "hhee" };
   st.setItem("test1", testBed); //TODO: mock this
   var test = st.getItem("test1");
   expect(test).toMatchObject(testBed);
 });
-
-
 
 test('storage sets an string as a string', () => {
   var testBed = "i am string";
@@ -15,7 +13,6 @@ test('storage sets an string as a string', () => {
   var test = st.getItem("test1");
   expect(test).toBe("i am string");
 });
-
 
 test('storage removes things', () => {
   var testBed = "i am string";
