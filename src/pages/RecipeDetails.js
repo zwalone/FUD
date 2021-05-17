f59687d2ecd7212468152966f8be361caf76670c
Madoxen <pchlad5@gmail.com>
2021-05-17 20:05:57 +0200

    Switched delete/add icon to reflect the actual state

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -5,7 +5,6 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import CloseIcon from '@material-ui/icons/Close';
 import LinkIcon from '@material-ui/icons/Link';
 import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -127,7 +126,7 @@ export default function RecipeDetails() {
             {/* Add Favorites button */}
 
             <Fab onClick={onFABClick} className={styles.floatingButton} color="secondary">
-                {isFavourite ? <FavoriteIcon /> : <DeleteIcon />}
+                {isFavourite ? <DeleteIcon /> : <FavoriteIcon />}
             </Fab>
         </div>
     )
