207502d81372a71f6f8ebbb19d273faf3e0c1bb2
Madoxen <pchlad5@gmail.com>
2021-05-17 15:31:46 +0200

    Moved API keys to dotenv

---
+++ src/data/Settings.js
@@ -0,0 +1,3 @@
+// Copy these two lines to AppKeys.js file and insert there your APP ID and APP KEY.
+export const appID = process.env.REACT_APP_appID;    // INSERT YOUR APP ID HERE
+export const appKey = process.env.REACT_APP_appKey;  // INSERT YOUR APP KEY HERE
