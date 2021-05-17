02ba920f8895c9398b1c218cd76d16e8bff50a6a
Madoxen <pchlad5@gmail.com>
2021-05-17 20:16:31 +0200

    Copied search page as favourites page

--- src/pages/FavoritePage.js
+++ src/pages/FavoritePage.js
@@ -1,17 +1,49 @@
+import { useContext } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { Fragment } from 'react';
-import CustomAppBar from '../components/CustomAppBar';
+import * as st from '../storage';
+import { Grid } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+import RecipeCard from '../components/RecipeCard';
+import { RecipeDataContext } from '../data/RecipeDataContext';
 
 export default function FavoritePage() {
     const classes = useStyles();
+    const fv = st.getItem("favourites");
+    const { recipes, setPhrase, setCurrentRecipe } = useContext(RecipeDataContext);
+    const history = useHistory();
 
-    return(
-        <Fragment>
-            <CustomAppBar onSearch={() => {}}/>
+    const OnClickItem = (item) => {
+        setCurrentRecipe(item);
+        history.push({ pathname: '/recipeDetails' })
+    }
+
+    const createRecipeDetails = () => {
+        if (fv === null)
+            return [];
+        var result = [];
+        for (let key in fv) {
+            let val = fv[key];
+            result.push(
+
+                <Grid item xs={6} key={val.url}>
+                    <RecipeCard
+                        title={val.label} subTitle={val.calories}
+                        image={val.image} key={val.url}
+                        onClick={() => OnClickItem(val)} />
+                </Grid>
+            )
+        }
+        return result;
+    }
+
+    return (
+        <>
             <div className={classes.root}>
-                <p>Favorite Page</p>
+                <Grid container spacing={1} justify='center'>
+                    {createRecipeDetails()}
+                </Grid>
             </div>
-        </Fragment>
+        </>
     )
 }
 
