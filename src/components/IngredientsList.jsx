dac84460e69fb6d4578edf92aa9eedd72d60e5ff
Madoxen <pchlad5@gmail.com>
2021-05-20 10:54:40 +0200

    Removed console log

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -13,8 +13,6 @@ export function IngredientsList({ ingredients, setIngredients, checkable }) {
     });
   }
 
-  console.log(ingredients)
-
   return (
     <div style={{ width: "100%" }}>
       {ingredients.map((ingredient, i) => (
